37b7bce1816855ae9281f593f679077289a75a40
Letolim <nikita.knoefel@gmail.com>
2026-02-16 20:23:02 +0000

    Delete RayCasting directory

--- RayCasting/RayLight.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class RayLight : MonoBehaviour
-{
-    public float distance;
-    public Vector3 color;
-}
